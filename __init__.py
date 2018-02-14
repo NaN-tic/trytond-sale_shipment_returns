cc60d420afa9d186a3ead79bb272f2728d04ecb4
C?dric Krier <ced@b2ck.com>
2018-02-14 17:28:39 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .shipment import *
+from . import shipment
+
 
 def register():
     Pool.register(
-        CreateSaleReturn,
+        shipment.CreateSaleReturn,
         module='sale_shipment_returns', type_='wizard')
