66dfd225f3db4a5f27f495060793e4d3ba656bb1
?ngel ?lvarez <angel@nan-tic.com>
2019-09-16 11:01:01 +0200

    adapt to 5.1

--- shipment.py
+++ shipment.py
@@ -4,6 +4,8 @@
 from trytond.pool import Pool
 from trytond.wizard import Wizard, StateAction
 from trytond.transaction import Transaction
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
 
 __all__ = ['CreateSaleReturn']
 
@@ -12,18 +14,6 @@ class CreateSaleReturn(Wizard):
     'Create Sale from Customer Return Shipment'
     __name__ = 'stock.sale.return.create'
     start = StateAction('sale.act_sale_form')
-
-    @classmethod
-    def __setup__(cls):
-        super(CreateSaleReturn, cls).__setup__()
-        cls._error_messages.update({
-                'shipment_done_title': 'You can not create return sale',
-                'shipment_done_msg': ('The return shipment with code "%s" is '
-                    'not yet sent.'),
-                'shipment_description': ('Shipment Out Return "%s"'),
-                'shipment_out_origin': ('Shipment Out Return "%s" origin does '
-                    'not come from a Shipment Out'),
-                })
 
     def do_start(self, action):
         pool = Pool()
@@ -39,9 +29,10 @@ class CreateSaleReturn(Wizard):
         sales = []
         for shipment_out_return in shipment_out_returns:
             if shipment_out_return.state != 'done':
-                self.raise_user_error('shipment_done_title',
+                raise UserError(gettext(
+                    'sale_shipment_returns.hipment_done_title',
                         error_description='shipment_done_msg',
-                        error_description_args=shipment_out_return.code)
+                        error_description_args=shipment_out_return.code))
 
             sale_origin = None
             if isinstance(shipment_out_return.origin, ShipmentOut):
@@ -53,8 +44,8 @@ class CreateSaleReturn(Wizard):
                 party = sale_origin.party
             else:
                 party = shipment_out_return.customer
-            description = self.raise_user_error('shipment_description',
-                (shipment_out_return.code), raise_exception=False)
+            description = gettext('sale_shipment_returns.hipment_description',
+                shipment=shipment_out_return.code)
 
             # create sale and lines from moves, and new origin move
             sale = Sale.get_sale_data(party, description)
@@ -81,8 +72,10 @@ class CreateSaleReturn(Wizard):
                         move.origin = 'sale.line,%s' % line.id
                         moves_to_save.append(move)
             if not lines:
-                self.raise_user_error('shipment_out_origin', (
-                    shipment_out_return.rec_name,))
+                raise UserError(gettext(
+                    'sale_shipment_returns.shipment_out_origin',
+                    shipment=shipment_out_return.rec_name))
+
             sale.lines = lines
             sales.append(sale)
 
